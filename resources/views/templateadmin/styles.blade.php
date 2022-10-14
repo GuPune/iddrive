2d6e085d8d07252dcebb72797a223a343f473e9f
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-10-14 14:05:44 +0700

    update
    update

---
+++ resources/views/templateadmin/styles.blade.php
@@ -0,0 +1,5 @@
+
+
+<link href="{!! asset('vendors/typicons.font/font/typicons.css') !!}" rel="stylesheet" type="text/css">
+<link href="{!! asset('vendors/css/vendor.bundle.base.css') !!}" rel="stylesheet" type="text/css">
+<link href="{!! asset('css/vertical-layout-light/style.css') !!}" rel="stylesheet" type="text/css">
