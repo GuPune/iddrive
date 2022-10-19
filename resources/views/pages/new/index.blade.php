c8efa67cbe195849fff570a46d58a66d32565eeb
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-10-19 11:38:44 +0700

    Update index.blade.php

--- resources/views/pages/new/index.blade.php
+++ resources/views/pages/new/index.blade.php
@@ -40,7 +40,7 @@
 
                         @php
                         $link = env('APP_URL');
-      $fineUrlImg = $link . "export/new/" . $items->n_code;
+      $fineUrlImg = $link . "/export/new/" . $items->n_code;
 dd($fineUrlImg);
     $datas = \App\CoreFunction\Cutstr::findimgInhtml($fineUrlImg) != NULL  ? \App\CoreFunction\Cutstr::findimgInhtml($fineUrlImg) : $link . "src/images/web/no-image-icon-11.png";
 
