676abe7f37e09915456d22f6e03d2581d0b927d8
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-10-20 14:50:02 +0700

    update
    update

---
+++ resources/views/template/testimonials.blade.php
@@ -0,0 +1,72 @@
+<section id="testimonials" class="testimonials section section-diff">
+    <div class="container" data-aos="fade-up">
+      <div class="section-title">
+        <h2>ข่าวสารและกิจกรรม</h2>
+        <p>ผลิตผลงานจากความต้องการเป็นหลัก เพื่อประโยชน์ที่คุ้มค่า.</p>
+      </div>
+
+      <div class="testimonials-slider swiper">
+        <div class="swiper-wrapper">
+
+          <div class="swiper-slide">
+            <div class="testimonial-wrap">
+
+                <div class="card">
+                    <img src="https://www.img.in.th/images/26d537bac29a32c0d72f7be139fb30da.jpg"  class="img-responsive" alt="Images" style="width: 100%; height:250px">
+                    <div class="card-body">
+                      <h3 class="card-title">3 สิ่งต้องห้าม ไม่ควรทำขณะขับรถลุยฝนตกหนัก</h3>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                      <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                  </div>
+
+
+            </div>
+
+          </div><!-- End testimonial item -->
+
+          <div class="swiper-slide">
+            <div class="testimonial-wrap">
+                <div class="card">
+                    <img src="https://www.img.in.th/images/26d537bac29a32c0d72f7be139fb30da.jpg"  class="img-responsive" alt="Images" style="width: 100%; height:250px">
+                    <div class="card-body">
+                      <h3 class="card-title">3 สิ่งต้องห้าม ไม่ควรทำขณะขับรถลุยฝนตกหนัก</h3>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                      <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                  </div>
+
+            </div>
+          </div><!-- End testimonial item -->
+
+          <div class="swiper-slide">
+            <div class="testimonial-wrap">
+                <div class="card">
+                    <img src="https://www.img.in.th/images/26d537bac29a32c0d72f7be139fb30da.jpg"  class="img-responsive" alt="Images" style="width: 100%; height:250px">
+                    <div class="card-body">
+                      <h3 class="card-title">3 สิ่งต้องห้าม ไม่ควรทำขณะขับรถลุยฝนตกหนัก</h3>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                      <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                  </div>
+
+            </div>
+          </div><!-- End testimonial item -->
+
+
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+      <div align="center" style="padding: 20px;color:white">
+        <a href="#" class="btn btn-large btn-danger"> ข่าวสารทั้งหมด </a>
+    </div>
+    </div>
+
+
+  </section><!-- End Testimonials Section -->
---
+++ resources/views/template/testproduct.blade.php
@@ -0,0 +1,82 @@
+<section id="testproduct" class="testimonials section section-diff section-bg-1">
+    <div class="container" data-aos="fade-up">
+      <div class="section-title">
+        <h2>สินค้าและบริการ</h2>
+        <p>ผลิตผลงานจากความต้องการเป็นหลัก เพื่อประโยชน์ที่คุ้มค่า.</p>
+      </div>
+
+      <div class="row bottom-gap">
+        <div class="col-sm-4">
+                   <!-- Latest work ITEM -->
+                   <div class="service fadeLeft" style="opacity: 1; left: 0px;padding:5px;">
+                     <div class="service-icon m-t-n">
+                       <a href="https://www.workbythai.com/blocks_in_page/184/เวิร์คบายไทย">
+                        <img data-original="https://www.workbythai.com//assets/images/article_184.png" alt="ภาพข่าวกิจกรรม" class="lazy" width="360" src="https://www.workbythai.com//assets/images/article_184.png" style="display: inline;">
+                    </a>
+                     </div>
+                     <div class="service-header">
+                        <h5 class="card-title">3 สิ่งต้องห้าม ไม่ควรทำขณะขับรถลุยฝนตกหนัก</h5>
+                     </div>
+                     <div class="service-desc">
+                           บริษัทเวิร์คบายไทย รับเขียนโปรแกรม รับทำเว็บไซต์ตามความต้องการของลูกค้า มีบริการนัดพบลูกค้านอกสถานทั้งกรุงเทพ และปริมณฑล    <br><font size="2px;" class="pull-right">01/11/2018</font>
+                     </div>
+                   </div>
+                   <!-- Latest work ITEM End -->
+                </div><div class="col-sm-4">
+
+                   <!-- Latest work ITEM -->
+                   <div class="service fadeLeft" style="opacity: 1; left: 0px;padding:5px;">
+                     <div class="service-icon m-t-n">
+                       <a href="https://www.workbythai.com/blocks_in_page/183/ พีทีที โกลบอล"><img data-original="https://www.workbythai.com//assets/images/article_183.png" alt="ภาพข่าวกิจกรรม" class="lazy" width="360" src="https://www.workbythai.com//assets/images/article_183.png" style="display: inline;"></a>
+                     </div>
+                     <div class="service-header">
+                        <h5 class="card-title">3 สิ่งต้องห้าม ไม่ควรทำขณะขับรถลุยฝนตกหนัก</h5>
+                     </div>
+                     <div class="service-desc">
+                           บริษัท เวิร์บายไทย ได้นำทีมผู้พัฒนาโปรแกรม เดินทางไปส่งมอบงานโปรแกรม และทำการจัดอบรมให้กับ บริษัท พีทีที โกลบอล เคมิคอล จำกัด จังหวัดระยอง      <br><font size="2px;" class="pull-right">25/10/2018</font>
+                     </div>
+                   </div>
+                   <!-- Latest work ITEM End -->
+                </div>
+                <div class="col-sm-4">
+                   <!-- Latest work ITEM -->
+                   <div class="service fadeLeft" style="opacity: 1; left: 0px;padding:5px;">
+                     <div class="service-icon m-t-n">
+                       <a href="https://www.workbythai.com/blocks_in_page/181/ส่งมอบเว็บไซต์"><img data-original="https://www.workbythai.com//assets/images/article_181.jpg" alt="ภาพข่าวกิจกรรม" class="lazy" width="360" src="https://www.workbythai.com//assets/images/article_181.jpg" style="display: inline;"></a>
+                     </div>
+                     <div class="service-header">
+                        <h5 class="card-title">3 สิ่งต้องห้าม ไม่ควรทำขณะขับรถลุยฝนตกหนัก</h5>
+                     </div>
+                     <div class="service-desc">
+                          บริษัท เวิร์คบายไทย อินเตอร์เน็ต แอนด์มาเก็ตติ้ง จำกัด นำทีม โดย นายทวิรัตน์ สาแก้ว รองกรรมการผู้จัดการเดินทาง มาจัดเทรนนิ่งให้กับ สำนักงานเกษตร ภายในจังหวัดสุรินทร์  <br><font size="2px;" class="pull-right">16/10/2018</font>
+                     </div>
+                   </div>
+                   <!-- Latest work ITEM End -->
+                </div>
+
+                <div class="col-sm-4">
+                    <!-- Latest work ITEM -->
+                    <div class="service fadeLeft" style="opacity: 1; left: 0px;padding:5px;">
+                      <div class="service-icon m-t-n">
+                        <a href="https://www.workbythai.com/blocks_in_page/181/ส่งมอบเว็บไซต์"><img data-original="https://www.workbythai.com//assets/images/article_181.jpg" alt="ภาพข่าวกิจกรรม" class="lazy" width="360" src="https://www.workbythai.com//assets/images/article_181.jpg" style="display: inline;"></a>
+                      </div>
+                      <div class="service-header">
+                         <h5 class="card-title">3 สิ่งต้องห้าม ไม่ควรทำขณะขับรถลุยฝนตกหนัก</h5>
+                      </div>
+                      <div class="service-desc">
+                           บริษัท เวิร์คบายไทย อินเตอร์เน็ต แอนด์มาเก็ตติ้ง จำกัด นำทีม โดย นายทวิรัตน์ สาแก้ว รองกรรมการผู้จัดการเดินทาง มาจัดเทรนนิ่งให้กับ สำนักงานเกษตร ภายในจังหวัดสุรินทร์  <br><font size="2px;" class="pull-right">16/10/2018</font>
+                      </div>
+                    </div>
+                    <!-- Latest work ITEM End -->
+                 </div>
+
+         </div>
+
+      <div align="center" style="padding: 20px;color:white">
+        <a href="#" class="btn btn-large btn-danger"> สินค้าและบริการทั้งหมด </a>
+    </div>
+
+    </div>
+
+
+  </section><!-- End Testimonials Section -->
