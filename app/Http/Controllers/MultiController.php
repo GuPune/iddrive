4c2ee6752bed122fb62f756d9c21437b64e85988
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-10-21 16:38:13 +0700

    update
    update

--- app/Http/Controllers/MultiController.php
+++ app/Http/Controllers/MultiController.php
@@ -11,9 +11,12 @@ class MultiController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request,$id)
     {
         //
+        dd($id);
+
+        return ('multi');
     }
 
     /**
