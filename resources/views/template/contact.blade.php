a6accd20293ed2435b2cf9f919e378b5079c6395
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-10-27 17:29:10 +0700

    update
    update

--- resources/views/template/contact.blade.php
+++ resources/views/template/contact.blade.php
@@ -2,8 +2,7 @@
     <div class="container" data-aos="fade-up">
 
       <div class="section-title">
-        <h2>Contact</h2>
-        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.</p>
+        <h2>ติดต่อเรา</h2>
       </div>
 
       <div class="row" data-aos="fade-up" data-aos-delay="100">
@@ -13,23 +12,9 @@
           <div class="row">
             <div class="col-md-12">
               <div class="info-box">
-                <i class="bx bx-map"></i>
-                <h3>Our Address</h3>
-                <p>A108 Adam Street, New York, NY 535022</p>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="info-box mt-4">
-                <i class="bx bx-envelope"></i>
-                <h3>Email Us</h3>
-                <p>info@example.com<br>contact@example.com</p>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="info-box mt-4">
-                <i class="bx bx-phone-call"></i>
-                <h3>Call Us</h3>
-                <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
+                <div class="contact-image">
+                    <img src="https://iddrives.co.th/web/src/images/web/map_iddrives.png" class="img-responsive" style="width: 100%;" alt="Smiling Two Girls">
+                </div>
               </div>
             </div>
           </div>
--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -5,13 +5,13 @@
         <div class="row">
 
           <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>Presento<span>.</span></h3>
+            <h3>บริษัท ไอดีไดรฟ์ จำกัด<span></span></h3>
             <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+                200/217 ถนน ชัยพฤกษ์<br>
+                ตำบลในเมือง อำเภอเมืองขอนแก่น<br>
+              ขอนแก่น 40000<br><br>
+              <strong>Phone:</strong> 043 224 000<br>
+              <strong>Email:</strong> -<br>
             </p>
           </div>
 
@@ -55,14 +55,7 @@
 
       <div class="me-md-auto text-center text-md-start">
         <div class="copyright">
-          &copy; Copyright <strong><span>Presento</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/presento-bootstrap-corporate-template/ -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          &copy; Copyright <strong><span>©</span></strong> บริษัท ไอดีไดรฟ์ จำกัด
         </div>
       </div>
       <div class="social-links text-center text-md-end pt-3 pt-md-0">
