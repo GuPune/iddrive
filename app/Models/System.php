501e7a43fb9817b11281826c031ef7552976ce1a
Rkknoob <iboonkhet@gmail.com>
2022-10-28 15:05:50 +0700

    update
    update

---
+++ app/Models/System.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class System extends Model
+{
+    use HasFactory;
+
+    protected $table = 'system_menu';
+
+    protected $fillable = [
+        'id',
+        'name_th',
+        'name_en',
+        'name_ch',
+        'system_menu',
+        'link',
+        'system_encodeid',
+    ];
+}
+
